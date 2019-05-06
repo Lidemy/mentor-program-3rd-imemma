b18223c1faa71a875394f9436f0ef13dec952dc5
imemma <lyris1003@gmail.com>
2019-05-07 00:12:57 +0800

    modify week2 HW

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  return str;
+  let tempStr = '';
+  for (let i = str.length; i >= 0; i -= 1) {
+    tempStr += str.charAt(i);
+  }
+  return tempStr;
 }
-
-reverse('hello');
+console.log(reverse('hello'));
+console.log(reverse('1abc2'));
+console.log(reverse('1,2,3,2,1'));
