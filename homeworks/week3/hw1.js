3a0058277f2c8c9615830d5f54db8c1a412a7bb6
imemma <lyris1003@gmail.com>
2019-05-16 23:43:37 +0800

    finished hw1

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,5 +1,11 @@
 function stars(n) {
-  console.log(n);
+  const result = [];
+  if (typeof n !== 'number') return '';
+  if (n < 1 || n > 30) return '';
+  for (let i = 1; i <= n; i += 1) {
+    result.push('*'.repeat(i));
+  }
+  return result;
 }
 
 module.exports = stars;
--- homeworks/week3/hw1.test.js
+++ homeworks/week3/hw1.test.js
@@ -1,7 +1,22 @@
 const stars = require('./hw1');
 
 describe('hw1', () => {
+  it('should return correct answer when n = 0', () => {
+    expect(stars(0)).toEqual('');
+  });
   it('should return correct answer when n = 1', () => {
     expect(stars(1)).toEqual(['*']);
+  });
+  it('should return correct answer when n = 3', () => {
+    expect(stars(3)).toEqual(['*', '**', '***']);
+  });
+  it('should return correct answer when n = 6', () => {
+    expect(stars(6)).toEqual(['*', '**', '***', '****', '*****', '******']);
+  });
+  it('should return correct answer when n = 100', () => {
+    expect(stars(100)).toEqual('');
+  });
+  it('should return correct answer when n = "hello"', () => {
+    expect(stars('hello')).toEqual('');
   });
 });
